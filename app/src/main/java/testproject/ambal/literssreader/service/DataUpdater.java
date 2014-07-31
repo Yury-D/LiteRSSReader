141e3a9c4a8d539db8bc43b2840cb7cff499e4ac
Yury-D <akm9000@tut.by>
2014-07-31 14:37:44 +0300

    подправлено оформление

--- app/src/main/java/testproject/ambal/literssreader/service/DataUpdater.java
+++ app/src/main/java/testproject/ambal/literssreader/service/DataUpdater.java
@@ -70,63 +70,58 @@ public class DataUpdater extends AsyncTask<String, Integer, Map<String, Integer>
             Parser parser = new Parser();
             String stringDownloadedChannel = mDownloader.download(url);
 
-            //если что нибудь скачалось, пытаемся парсить
-            save:
-            if (stringDownloadedChannel.length() != 0) {
-                Channel downloadedChannel = null;
-                try {
-                    downloadedChannel = parser.parse(stringDownloadedChannel);
-                } catch (NullPointerException e) {
-                    e.printStackTrace();
-                    Log.e(LOG_TAG, mContext.getString(R.string.error_message));
-                    break save;
-                }
-                downloadedChannel.setUrl(url);
-                //проверяем, есть ли такой фид в базе
-                List<Channel> sameChannels = Collections.EMPTY_LIST;
-                List<Channel> samePubDates = Collections.EMPTY_LIST;
-                try {
-                    sameChannels = HelperFactory.getHelper().getChannelDao().queryForEq("title",
-                            downloadedChannel.getTitle());
-                    samePubDates = HelperFactory.getHelper().getChannelDao().queryForEq("lastBuildDate",
-                            downloadedChannel.getLastBuildDate());
-                } catch (SQLException e) {
-                    e.printStackTrace();
-                }
-
-                boolean sameChanelDetected = sameChannels.size() > 0; //если >0, то в базе уже есть фид с таким заголовком
-                boolean laterPubDateDetected = !(samePubDates.size() > 0);//если >0, то дата публикации одинакова
-                //добавляем в базу, если такого фида нет, либо если фид есть, и дата публикации более поздняя
-                if (!sameChanelDetected || (sameChanelDetected & (laterPubDateDetected))) {
-                    //сохраняем в базу данные о фиде
+            //проверяем, что скачалось хоть что-то
+            if (stringDownloadedChannel != null && (stringDownloadedChannel.length() != 0)) {
+                Channel downloadedChannel = parser.parse(stringDownloadedChannel);
+                // что это что-то удалось распарсить
+                if (downloadedChannel != null) {
+                    downloadedChannel.setUrl(url);
+                    //проверяем, есть ли такой фид в базе
+                    List<Channel> sameChannels = Collections.emptyList();
+                    List<Channel> samePubDates = Collections.emptyList();
                     try {
-                        //предварительно удалив старый фид с таким же заголовком и все его айтемы
-                        int oldId = 0;
-                        if (sameChanelDetected) {
-                            updateCounter++;
-                            createCounter--;
-                            oldId = HelperFactory.getHelper().getChannelDao().extractId(sameChannels.get(0));
-                            List<Item> itemsForDelete = HelperFactory.getHelper().getItemDao().queryForEq("channel_id",
-                                    sameChannels.get(0).getId());
-                            HelperFactory.getHelper().getItemDao().delete(itemsForDelete);
-                            HelperFactory.getHelper().getChannelDao().deleteById(sameChannels.get(0).getId());
-                        }
-
-                        //чтобы id не менялись при апдейте, сохраняем старый и здесь применяем его к новому
-                        downloadedChannel.setId(oldId);
-                        HelperFactory.getHelper().getChannelDao().create(downloadedChannel);
-                        createCounter++;
-
+                        sameChannels = HelperFactory.getHelper().getChannelDao().queryForEq("title",
+                                downloadedChannel.getTitle());
+                        samePubDates = HelperFactory.getHelper().getChannelDao().queryForEq("lastBuildDate",
+                                downloadedChannel.getLastBuildDate());
                     } catch (SQLException e) {
                         e.printStackTrace();
                     }
-                    //сохраняем в базу айтемы фида - заголовки новостей
-                    Collection<Item> items = downloadedChannel.getItems();
-                    for (Item item : items) {
+
+                    boolean sameChanelDetected = sameChannels.size() > 0; //если >0, то в базе уже есть фид с таким заголовком
+                    boolean laterPubDateDetected = !(samePubDates.size() > 0);//если >0, то дата публикации одинакова
+                    //добавляем в базу, если такого фида нет, либо если фид есть, и дата публикации более поздняя
+                    if (!sameChanelDetected || (sameChanelDetected & (laterPubDateDetected))) {
+                        //сохраняем в базу данные о фиде
                         try {
-                            HelperFactory.getHelper().getItemDao().create(item);
+                            //предварительно удалив старый фид с таким же заголовком и все его айтемы
+                            int oldId = 0;
+                            if (sameChanelDetected) {
+                                updateCounter++;
+                                createCounter--;
+                                oldId = HelperFactory.getHelper().getChannelDao().extractId(sameChannels.get(0));
+                                List<Item> itemsForDelete = HelperFactory.getHelper().getItemDao().queryForEq("channel_id",
+                                        sameChannels.get(0).getId());
+                                HelperFactory.getHelper().getItemDao().delete(itemsForDelete);
+                                HelperFactory.getHelper().getChannelDao().deleteById(sameChannels.get(0).getId());
+                                //чтобы id не менялись при апдейте, сохраняем старый и здесь применяем его к новому
+                                downloadedChannel.setId(oldId);
+                            }
+
+                            HelperFactory.getHelper().getChannelDao().create(downloadedChannel);
+                            createCounter++;
+
                         } catch (SQLException e) {
                             e.printStackTrace();
+                        }
+                        //сохраняем в базу айтемы фида - заголовки новостей
+                        Collection<Item> items = downloadedChannel.getItems();
+                        for (Item item : items) {
+                            try {
+                                HelperFactory.getHelper().getItemDao().create(item);
+                            } catch (SQLException e) {
+                                e.printStackTrace();
+                            }
                         }
                     }
                 }
